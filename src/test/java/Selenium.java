e73e8f993ec4af1f53276e15c95d469c3d459b32
MaycolnDias <maycoln.works@gmail.com>
2020-06-15 22:06:28 -0300

    Criando before e after

--- src/test/java/Selenium.java
+++ src/test/java/Selenium.java
@@ -9,18 +9,18 @@ import java.util.Random;
 
 public class Selenium {
     //Criar instancia do driver do chrome
-    private WebDriver driver = new ChromeDriver();
+    private static WebDriver driver = new ChromeDriver();
 
     @BeforeAll
-    public void setup(){
+    public static void setup(){
         //Setar as propriedades do chrome Driver
-        System.setProperty("./src/main/resources/chromedriver","chromedriver");
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
         //Abrir browser e acessar a URL
         driver.get("https://automacaocombatista.herokuapp.com");
         //Maximizando o Browser Windows
         //driver.manage().window().maximize();
 
-        driver.findElement(By.xpath("//a[text()='Começar Automação Web']")).click();
+        driver.findElement(By.xpath("//a[text()='ComeÃ§ar AutomaÃ§Ã£o Web']")).click();
         //Maximizando o Browser Mac/Linux
         driver.manage().window().fullscreen();
     }
@@ -32,8 +32,8 @@ public class Selenium {
         String email = random.nextInt(10000)+"@bootcamp.com.br";
 
         //Identificando um elemento e preenchendo o campo com um texto
-        driver.findElement(By.xpath("//a[text()='Formulário']")).click();
-        driver.findElement(By.xpath("//a[text()='Criar Usuários']")).click();
+        driver.findElement(By.xpath("//a[text()='FormulÃ¡rio']")).click();
+        driver.findElement(By.xpath("//a[text()='Criar UsuÃ¡rios']")).click();
         driver.findElement(By.id("user_name")).sendKeys("Marcos");
         driver.findElement(By.id("user_lastname")).sendKeys("Barbosa");
         driver.findElement(By.id("user_email")).sendKeys(email);
@@ -46,11 +46,11 @@ public class Selenium {
         //Clicando no botao Criar
         driver.findElement(By.name("commit")).click();
 
-        //Atribuindo o texto da mensagem na tela para a variável
+        //Atribuindo o texto da mensagem na tela para a variÃ¡vel
         String mensagem = driver.findElement(By.xpath("//p[@id='notice']")).getText();
 
-        //Validando se a mensagem que eu necessito está sendo exibida na tela
-        Assertions.assertEquals("Usuário Criado com sucesso", mensagem);
+        //Validando se a mensagem que eu necessito estÃ¡ sendo exibida na tela
+        Assertions.assertEquals("UsuÃ¡rio Criado com sucesso", mensagem);
     }
 
     @Test
@@ -62,7 +62,7 @@ public class Selenium {
     }
 
     @AfterAll
-    public void fecharBrowser(){
+    public static void fecharBrowser(){
         //Fechando meu navagador
         driver.quit();
     }
